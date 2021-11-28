37b9e1639edc04bd9063aee5ba80c5cdd32e27be
hexsix <hexsixology@gmail.com>
2021-11-28 16:57:41 +0800

    fix

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def download() -> Any:
             time.sleep(6)
     if not rss_json:
         raise Exception('Failed to download RSS.')
-    print('Succeed to download RSS.')
+    print('Succeed to download RSS.\n')
     return rss_json
 
 
@@ -44,12 +44,15 @@ def parse(rss_json: Dict) -> List[Dict[str, Any]]:
             item = dict()
             item['post_url'] = entry['link']
             item['post_id'] = item['post_url'].split('/')[-1]
-            item['score'] = int(re.search(r'Score:\d*', entry['description']).group().split(':')[-1])
-            item['sample'] = re.search(r'https://files.yande.re/sample/[^"]*', entry['description']).group()
+            item['score'] = int(
+                re.search(r'Score:\d*', entry['description']).group().split(':')[-1])
+            item['sample'] = re.search(
+                r'https://files.yande.re/sample/[^"]*', entry['description']).group()
             items.append(item)
-        except:
+        except Exception as e:
+            print(f'Exception: {e}')
             continue
-    print(f"Parse RSS End. {len(items)}/{len(rss_json['entries'])} Succeed.")
+    print(f"Parse RSS End. {len(items)}/{len(rss_json['entries'])} Succeed.\n")
     return items
 
 
@@ -62,6 +65,7 @@ def filter(item: Dict[str, Any]) -> bool:
 
 
 def send(photo: str, caption: str) -> bool:
+    print(f'Send pid: {caption} ...')
     target = f"https://api.telegram.org/bot{os.environ['TG_TOKEN']}/sendPhoto"
     params = {
         'chat_id': os.environ['CHAT_ID'],
@@ -75,9 +79,27 @@ def send(photo: str, caption: str) -> bool:
         if response.json()['ok']:
             print(f'Succeed to send {caption}.')
             return True
-    except:
+        else:
+            print(f'Telegram api returns {response.json()}')
+            print(f'photo: {photo}, caption: {caption}')
+    except Exception as e:
+        print(f'Exception: {e}')
         pass
-    print(f'Failed to send {caption}.')
+    print(f'Failed to send {caption}.\n')
+    return False
+
+
+def redis_set(post_id: str) -> bool:
+    for retry in range(5):
+        print(f'The {retry + 1}th attempt to set redis, 5 attempts in total.')
+        try:
+            if REDIS.set(post_id, 'sent', ex=2678400):  # expire after a month
+                print(f'Succeed to set redis {post_id}.\n')
+                return True
+        except:
+            print('Failed to set redis, the next attempt will start in 6 seconds.')
+            time.sleep(6)
+    print(f'Failed to set redis, {post_id} may be sent twice.\n')
     return False
 
 
@@ -86,11 +108,11 @@ def main():
     rss_json = download()
     items = parse(rss_json)
     filtered_items = [item for item in items if not filter(item)]
-    print(f'{len(filtered_items)}/{len(items)} Filter.')
+    print(f'{len(filtered_items)}/{len(items)} filtered by already sent / score threshold.\n')
     count = 0
     for item in filtered_items:
         if send(item['sample'], item['post_url']):
-            REDIS.set(item['post_id'], 'sent', ex=2678400)  # expire after a month
+            redis_set(item['post_id'])
             count += 1
     print(f'{count}/{len(filtered_items)} Succeed.')
     print('============ App End ============')
